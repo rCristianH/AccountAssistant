574252f7bcac6be8e5835fcec1ac0e8accfecbe8
Cristian Ramirez <ccrhdbj@outlook.com>
2023-08-17 06:55:30 -0500

    Correcion facturacion

--- script.js
+++ script.js
@@ -90,7 +90,111 @@ function generateJSON() {
 
 // Función para generar el HTML a partir de los elementos del array
 function generateHTML(pedidos) {
-  return pedidos.map(item => `<div>${item.precio} ${item.nombre}</div>`).join("");
+  const productos = {}; // Objeto para acumular cantidades y totales por producto
+  let totalGeneral = 0; // Variable para acumular el total general
+
+  // Procesar los pedidos y acumular información en el objeto 'productos'
+  pedidos.forEach(item => {
+    if (!productos[item.nombre]) {
+      productos[item.nombre] = { cantidad: 0, precio: item.precio };
+    }
+    productos[item.nombre].cantidad++;
+    totalGeneral += item.precio;
+  });
+
+  // Crear el encabezado de la tabla
+  let html = `
+    <table>
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>P/Unidad</th>
+          <th>P/Total</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  // Agregar filas para cada producto acumulado
+  for (const nombreProducto in productos) {
+    const producto = productos[nombreProducto];
+    const subtotal = producto.cantidad * producto.precio;
+    html += `
+      <tr>
+        <td>${nombreProducto} X${producto.cantidad}</td>
+        <td>${producto.precio}</td>
+        <td>${subtotal}</td>
+      </tr>
+    `;
+  }
+
+  // Agregar fila para el total general
+  html += `
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td></td>
+          <td>${totalGeneral}</td>
+        </tr>
+      </tfoot>
+    </table>
+  `;
+
+  return html;
+}function generateHTML(pedidos) {
+  const productos = {}; // Objeto para acumular cantidades y totales por producto
+  let totalGeneral = 0; // Variable para acumular el total general
+
+  // Procesar los pedidos y acumular información en el objeto 'productos'
+  pedidos.forEach(item => {
+    if (!productos[item.nombre]) {
+      productos[item.nombre] = { cantidad: 0, precio: item.precio };
+    }
+    productos[item.nombre].cantidad++;
+    totalGeneral += item.precio;
+  });
+
+  // Crear el encabezado de la tabla
+  let html = `
+    <table>
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>P/Unidad</th>
+          <th>P/Total</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  // Agregar filas para cada producto acumulado
+  for (const nombreProducto in productos) {
+    const producto = productos[nombreProducto];
+    const subtotal = producto.cantidad * producto.precio;
+    html += `
+      <tr>
+        <td>${nombreProducto} X${producto.cantidad}</td>
+        <td>${producto.precio}</td>
+        <td>${subtotal}</td>
+      </tr>
+    `;
+  }
+
+  // Agregar fila para el total general
+  html += `
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td></td>
+          <td>${totalGeneral}</td>
+        </tr>
+      </tfoot>
+    </table>
+  `;
+
+  return html;
 }
 
 // Función para imprimir el JSON en la consola
