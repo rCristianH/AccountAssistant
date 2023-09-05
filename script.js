f795f2dc7fdc4fa9cafd4e6bede3adaa1456c516
Cristian Ramirez <ccrhdbj@outlook.com>
2023-09-05 09:51:47 -0500

    facturaPDF

--- script.js
+++ script.js
@@ -27,8 +27,13 @@ const productos = {
   ],
 };
 
+const jsPDFScript = document.createElement("script");
+jsPDFScript.src =
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js";
+jsPDFScript.type = "text/javascript";
 const elementMod = document.querySelector(".result > p > span");
 const facturaElement = document.getElementById("factura");
+const generarRecibo = document.querySelector("#generarRecibo");
 const unidadesElement = document.getElementById("unidades");
 const paquetesElement = document.getElementById("paquetes");
 const bebidasElement = document.getElementById("bebidas");
@@ -57,24 +62,24 @@ function otroPrecio() {
 }
 function agregaNombre() {
   let value = String(document.getElementsByClassName("name-input")[0].value);
-  return value
+  return value;
 }
-
 
 // Funcion que muestra u oculta la factura
 function botonPagar() {
   if (facturaElement.style.display === "none") {
     facturaElement.style.display = "flex";
+    generarRecibo.style.display = "block";
     updatePage();
   } else {
     facturaElement.style.display = "none";
+    generarRecibo.style.display = "none";
   }
 }
 
-
 // Función para actualizar el contenido en la página
 function updatePage() {
-  facturaElement.innerHTML = generateHTML(pedidos) + '<button id="generarRecibo" onclick="imprimirJSON()">Generar Recibo</button>';
+  facturaElement.innerHTML = generateHTML(pedidos);
 }
 
 // Función para generar el JSON a partir de los elementos del array
@@ -82,10 +87,10 @@ function generateJSON() {
   const factura = {
     fecha: new Date().toString(),
     nombreCliente: agregaNombre(),
-    pedido: pedidos
+    pedido: pedidos,
   };
   generateAndDownloadJSON(factura);
-  sendBack(factura)
+  sendBack(factura);
   return factura;
 }
 
@@ -95,7 +100,7 @@ function generateHTML(pedidos) {
   let totalGeneral = 0; // Variable para acumular el total general
 
   // Procesar los pedidos y acumular información en el objeto 'productos'
-  pedidos.forEach(item => {
+  pedidos.forEach((item) => {
     if (!productos[item.nombre]) {
       productos[item.nombre] = { cantidad: 0, precio: item.precio };
     }
@@ -104,60 +109,9 @@ function generateHTML(pedidos) {
   });
 
   // Crear el encabezado de la tabla
+  let optClient = agregaNombre() == "" ? "Cliente General" : agregaNombre();
   let html = `
-    <table>
-      <thead>
-        <tr>
-          <th>Producto</th>
-          <th>P/Unidad</th>
-          <th>P/Total</th>
-        </tr>
-      </thead>
-      <tbody>
-  `;
-
-  // Agregar filas para cada producto acumulado
-  for (const nombreProducto in productos) {
-    const producto = productos[nombreProducto];
-    const subtotal = producto.cantidad * producto.precio;
-    html += `
-      <tr>
-        <td>${nombreProducto} X${producto.cantidad}</td>
-        <td>${producto.precio}</td>
-        <td>${subtotal}</td>
-      </tr>
-    `;
-  }
-
-  // Agregar fila para el total general
-  html += `
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>Total</td>
-          <td></td>
-          <td>${totalGeneral}</td>
-        </tr>
-      </tfoot>
-    </table>
-  `;
-
-  return html;
-}function generateHTML(pedidos) {
-  const productos = {}; // Objeto para acumular cantidades y totales por producto
-  let totalGeneral = 0; // Variable para acumular el total general
-
-  // Procesar los pedidos y acumular información en el objeto 'productos'
-  pedidos.forEach(item => {
-    if (!productos[item.nombre]) {
-      productos[item.nombre] = { cantidad: 0, precio: item.precio };
-    }
-    productos[item.nombre].cantidad++;
-    totalGeneral += item.precio;
-  });
-
-  // Crear el encabezado de la tabla
-  let html = `
+  <div class="client-name">Cliente: <span class="client-name--span">${optClient}</span></div>
     <table>
       <thead>
         <tr>
@@ -199,18 +153,41 @@ function generateHTML(pedidos) {
 }
 
 // Función para imprimir el JSON en la consola
-function imprimirJSON() {
+/* function imprimirJSON() {
   const factura = generateJSON();
   console.log(JSON.stringify([factura], null, 2)); // Imprimir JSON en la consola
+} */
+function imprimirJSON() {
+  const element = document.querySelector("#factura");
+  html2canvas(element).then(function (canvas) {
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF();
+    console.log(pdf)
+    const pdfWidth = pdf.internal.pageSize.height;
+    console.log(pdfWidth)
+    pdf.addImage(
+      imgData,
+      "PNG",
+      0,
+      0,
+      210,
+      290,
+    );
+
+    // Descargar el PDF
+    pdf.save("mi-documento.pdf");
+  });
 }
 
 function generateAndDownloadJSON(data) {
-  const jsonBlob = new Blob([JSON.stringify([data], null, 2)], { type: 'application/json' });
+  const jsonBlob = new Blob([JSON.stringify([data], null, 2)], {
+    type: "application/json",
+  });
   const jsonUrl = URL.createObjectURL(jsonBlob);
 
-  const downloadLink = document.createElement('a');
+  const downloadLink = document.createElement("a");
   downloadLink.href = jsonUrl;
-  downloadLink.download = 'recibo.json';
+  downloadLink.download = "recibo.json";
 
   // Agrega el enlace al documento y haz clic en él para descargar el archivo
   document.body.appendChild(downloadLink);
@@ -222,21 +199,20 @@ function generateAndDownloadJSON(data) {
 
 const sendBack = async (factura) => {
   try {
-    const response = await fetch('url/guardar-json', {
-      method: 'POST',
+    const response = await fetch("url/guardar-json", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(factura)
+      body: JSON.stringify(factura),
     });
 
     const data = await response.text();
     console.log(data);
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 };
-
 
 //Funcion que oculta o muestra los tipos de productos
 function displaySection(section) {
