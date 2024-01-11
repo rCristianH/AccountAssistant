7883ca557f2584f86edaeceefa4b237ef75f2ef8
Cristian Ramirez <ccrhdbj@outlook.com>
2024-01-11 11:57:14 -0500

    jsPDF HTML2Canvas

--- src/pages/Factura/factura.js
+++ src/pages/Factura/factura.js
@@ -1,5 +1,7 @@
 import { orders } from "../../utils/Calls";
 import { addClientName } from "../../utils/utils";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 export const generateJSON = () => {
   const factura = {
@@ -14,15 +16,14 @@ export const generateJSON = () => {
 export const imprimirJSON = () => {
   const element = document.querySelector("#factura");
   element.style.width = "400px";
-  element.style.height = "800px"
+  element.style.height = "800px";
   element.style.fontSize = "20px";
   element.style.backgroundColor = "white";
   element.style.position = "static";
   element.style.borderRadius = "0";
   element.style.border = "none";
 
- 
-  html2canvas(element).then(canvas => {
+  html2canvas(element).then((canvas) => {
     const imgData = canvas.toDataURL("image/png");
     const pdf = new jsPDF();
     pdf.addImage(imgData, 0, 0, 210, 310);
@@ -38,5 +39,3 @@ export const imprimirJSON = () => {
   element.style.borderRadius = "10px";
   element.style.border = "1px solid black";
 };
-/* html2canvas
-jsPDF */
--- src/utils/Calls.js
+++ src/utils/Calls.js
@@ -1,7 +1,3 @@
-export const jsPDFScript = document.createElement("script");
-jsPDFScript.src =
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js";
-jsPDFScript.type = "text/javascript";
 export const facturaElement = document.getElementById("factura");
 export const orders = [];
 export const headerNavList = document.querySelector(".nav--header--ul")
