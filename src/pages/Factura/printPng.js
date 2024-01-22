6e911c3a94d3909183bf82b0f202b8c6a7c21eba
Cristian Ramirez <ccrhdbj@outlook.com>
2024-01-22 13:04:21 -0500

    romove  styles from js to css

--- src/pages/Factura/printPng.js
+++ src/pages/Factura/printPng.js
@@ -3,31 +3,15 @@ import { getTime } from "../../utils/getTime";
 
 export const printPng = () => {
   const element = document.querySelector("#factura");
-  element.style.width = "400px";
-  element.style.height = "800px";
-  element.style.fontSize = "20px";
-  element.style.backgroundColor = "white";
-  element.style.position = "static";
-  element.style.borderRadius = "0";
-  element.style.border = "none";
-
+  //add styles to enhance resoluction
+  element.classList.toggle("toggleAsideFactura");
   html2canvas(element).then((canvas) => {
     const imgData = canvas.toDataURL("image/png");
     const link = document.createElement("a");
     link.href = imgData;
     link.download = `bill_${getTime()}.png`;
     link.click();
-    /*  const pdf = new jsPDF();
-    pdf.addImage(imgData, 0, 0, 210, 310);
-
-    // Descargar el PDF
-    pdf.save(`bill_${getTime}.pdf`); */
+    /*  const pdf = new jsPDF(); pdf.addImage(imgData, 0, 0, 210, 310); pdf.save(`bill_${getTime}.pdf`); */
   });
-  element.style.width = "300px";
-  element.style.height = "78%";
-  element.style.fontSize = "14px";
-  element.style.backgroundColor = "var(--very-light-pink)";
-  element.style.position = "absolute";
-  element.style.borderRadius = "10px";
-  element.style.border = "1px solid black";
+  element.classList.toggle("toggleAsideFactura");
 };
