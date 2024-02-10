e3afad6adf72eb52c123da5811e703d6a70a7eff
Cristian Ramirez <ccrhdbj@outlook.com>
2024-02-10 16:07:47 -0500

    Agrega lógica para almacenar datos en el localStorage al recargar la página

--- src/utils/Products.js
+++ src/utils/Products.js
@@ -1,1 +1,20 @@
-export const productos = GetProducts.data
+//agrega la varibale data al localsotrage para que no se pierda al recargar la pagina
+
+const GetStock = () => {
+  const getDATALS = localStorage.getItem("data");
+  const dataLsParse = JSON.parse(localStorage.getItem("data"));
+  const data = GetProducts.data;
+  if (!getDATALS || getDATALS == 'null') {
+    localStorage.setItem("data", JSON.stringify(data));
+    return JSON.parse(localStorage.getItem("data"));
+  } else if (getDATALS.length > 4) {
+    if (getDATALS != JSON.stringify(data)) {
+      localStorage.setItem("data", JSON.stringify(data));
+      return JSON.parse(localStorage.getItem("data"));
+    } else {
+      return dataLsParse;
+    }
+  }
+};
+
+export let productos = GetStock();
