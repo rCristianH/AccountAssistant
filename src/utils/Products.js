d35e491dc0fd1e024d0c821a961a52c198a43169
Cristian Ramirez <ccrhdbj@outlook.com>
2024-02-11 11:15:05 -0500

    Agrega función isOnline para verificar la conexión y utiliza datos en caché si no hay conexión

--- src/utils/Products.js
+++ src/utils/Products.js
@@ -1,9 +1,21 @@
 //agrega la varibale data al localsotrage para que no se pierda al recargar la pagina
 
+export const isOnline = () => {
+  if (navigator.onLine) {
+    return true;
+  } else {
+    return false;
+  }
+}
 const GetStock = () => {
   const getDATALS = localStorage.getItem("data");
   const dataLsParse = JSON.parse(localStorage.getItem("data"));
   const data = GetProducts.data;
+
+  //aniade un condicional para si es GetProducts is not defined
+  if (isOnline==false) {
+    return JSON.parse(localStorage.getItem("data"));
+  }
   if (!getDATALS || getDATALS == 'null') {
     localStorage.setItem("data", JSON.stringify(data));
     return JSON.parse(localStorage.getItem("data"));
@@ -18,3 +30,4 @@ const GetStock = () => {
 };
 
 export let productos = GetStock();
+
