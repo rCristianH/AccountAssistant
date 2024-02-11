848efb486c3cb50d22bed682a9b2a12b92c711e9
Cristian Ramirez <ccrhdbj@outlook.com>
2024-02-11 11:34:48 -0500

    Refactorizar Products.js para corregir problemas de almacenamiento local y conexión en línea

--- src/utils/Products.js
+++ src/utils/Products.js
@@ -1,6 +1,6 @@
 //agrega la varibale data al localsotrage para que no se pierda al recargar la pagina
 
-export const isOnline = () => {
+const isOnline = () => {
   if (navigator.onLine) {
     return true;
   } else {
@@ -10,18 +10,17 @@ export const isOnline = () => {
 const GetStock = () => {
   const getDATALS = localStorage.getItem("data");
   const dataLsParse = JSON.parse(localStorage.getItem("data"));
-  const data = GetProducts.data;
 
   //aniade un condicional para si es GetProducts is not defined
   if (isOnline==false) {
     return JSON.parse(localStorage.getItem("data"));
   }
   if (!getDATALS || getDATALS == 'null') {
-    localStorage.setItem("data", JSON.stringify(data));
+    localStorage.setItem("data", JSON.stringify(GetProducts.data));
     return JSON.parse(localStorage.getItem("data"));
   } else if (getDATALS.length > 4) {
-    if (getDATALS != JSON.stringify(data)) {
-      localStorage.setItem("data", JSON.stringify(data));
+    if (getDATALS != JSON.stringify(GetProducts.data)) {
+      localStorage.setItem("data", JSON.stringify(GetProducts.data));
       return JSON.parse(localStorage.getItem("data"));
     } else {
       return dataLsParse;
